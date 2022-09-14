d9e8abedb74d89f0d2f56085cb805a9b05d65788
Oleksii_Chernykh <84220570+alexey-chernykh@users.noreply.github.com>
2022-09-14 13:57:29 +0300

    importing files #2

---
+++ Repositories/PlayerRepository.cs
@@ -0,0 +1,19 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using Repositories.Abstract;
+using Repositories.Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public class PlayerRepository : DbRepository<Player>, IPlayerRepository
+    {
+        public PlayerRepository(DbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ ViewModels/PlayerVM.cs
@@ -0,0 +1,27 @@
+﻿using Entities;
+
+namespace ViewModels
+{
+    public class PlayerVM
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string? Sex { get; set; }
+        public string? Country { get; set; }
+        public string? HeroType { get; set; }
+        public int? Experience { get; set; }
+        public PlayerVM(Player player)
+        {
+            Id = player.Id;
+            Name = player.Name;
+            Email = player.Email;
+            BirthDate = player.BirthDate;
+            Sex = player.Sex;
+            Country = player.Country;
+            HeroType = player.HeroType;
+            Experience = player.Experience;
+        }
+    }
+}
